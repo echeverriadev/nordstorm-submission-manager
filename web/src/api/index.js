279c716a7bf710cf28fe8ecf6f651f8dc2dc98a8
Kevin Garcia <kevin93ps@gmail.com>
2019-04-19 17:47:18 -0400

    Import button working

--- web/src/api/index.js
+++ web/src/api/index.js
@@ -1,9 +1,5 @@
 import { apiFetch } from './apiFetch'
 
-const PORT_BACKEND = process.env.REACT_APP_PORT_BACKEND || '8081';
-const URL_BACKEND = process.env.REACT_APP_API_URL_BACKEND || 'http://localhost';
-export const API_URL_BACKEND = `${URL_BACKEND}:${PORT_BACKEND}`
-
 export const getCyclesApi = () => apiFetch(`/cycles`)
 export const getDivisionsApi = () => apiFetch(`/divisions`)
 export const getItemsApi = (start, end, divisionId, cycleId) => apiFetch(`/items?range=[${start},${end}]&divisionId=${divisionId}&cycleId=${cycleId}`)
