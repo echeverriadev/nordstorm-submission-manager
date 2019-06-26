99f964c82be59db4fe4152b3a66f9ec017ce0fda
Carlos <echeverriacarlos1@gmail.com>
2019-06-26 14:59:49 -0400

    NDSM-80: completed

--- api/src/index.js
+++ api/src/index.js
@@ -9,7 +9,10 @@ let app = express();
 
 const port = process.env.PORT;
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
